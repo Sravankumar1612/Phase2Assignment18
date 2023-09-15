2ec5563ac811b8b1af46abe4dec71828f2995560
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-09-15 08:52:03 +0530

    intial commit

---
+++ Data/MoviessDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment18.Models;
+
+namespace Assignment18.Data
+{
+    public class MoviessDbContext : DbContext
+    {
+        public MoviessDbContext (DbContextOptions<MoviessDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment18.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment18.Models
+{
+
+     [Table("Movie")]
+    public class Movie
+    {
+            [Key]
+            public int Id { get; set; }
+            [Required]
+            public string Title { get; set; }
+            [Required]
+            public string Genre { get; set; }
+            [Required]
+            public string Director { get; set; }
+    }
+    
+}
